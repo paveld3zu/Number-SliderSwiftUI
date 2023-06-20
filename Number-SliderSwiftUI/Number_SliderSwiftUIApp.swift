2f9de427cfce01da07e74a3bac586f43ca79324d
paveld3zu <pavel.karunnyj@yandex.ru>
2023-06-20 15:16:47 +0300

    Initial Commit

---
+++ Number-SliderSwiftUI/Number_SliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Number_SliderSwiftUIApp.swift
+//  Number-SliderSwiftUI
+//
+//  Created by Pavel Karunnyj on 20.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Number_SliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
